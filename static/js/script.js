df5d553b50752d38824633912dca0ad2ef986336
Damian McNulty <damianmcnulty@me.com>
2019-02-05 21:23:50 +0000

    recipes

--- static/js/script.js
+++ static/js/script.js
@@ -20,10 +20,10 @@ $(document).ready(function () {
     $('.btn-filter').on('click', function () {
       var $target = $(this).data('target');
       if ($target !== 'all') {
-        $('.table tr').css('display', 'none');
-        $('.table tr[data-status="' + $target + '"]').fadeIn('slow');
+        $('.panel-body').css('display', 'none');
+        $('.panel-body[data-status="' + $target + '"]').fadeIn('slow');
       } else {
-        $('.table tr').css('display', 'none').fadeIn('slow');
+        $('.panel-body').css('display', 'none').fadeIn('slow');
       }
     });
 
